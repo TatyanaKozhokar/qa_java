3f8e29143383e3d4203c77969e8757751ffab4a6
TatyanaKozhokar <tz5799@gmail.com>
2025-05-15 15:44:28 +0300

    Commit

--- src/main/java/com/example/Feline.java
+++ src/main/java/com/example/Feline.java
@@ -3,20 +3,29 @@ package com.example;
 import java.util.List;
 
 public class Feline extends Animal implements Predator {
+    Animal animal;
+
+    public Feline(Animal animal) {
+        this.animal = animal;
+    }
 
     @Override
     public List<String> eatMeat() throws Exception {
         return getFood("Хищник");
     }
 
+
+
     @Override
     public String getFamily() {
         return "Кошачьи";
     }
 
+
     public int getKittens() {
         return getKittens(1);
     }
+
 
     public int getKittens(int kittensCount) {
         return kittensCount;
